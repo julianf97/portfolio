7a86dd457e3a84bda3266e0d8cdd12e9e5444c8d
Julian Finelli <julianfinelli97@gmail.com>
2024-02-19 09:16:04 -0300

    chore: ultimos cambios

--- src/pages/ProyectDetail/ProyectDetail/ProyectDetail.jsx
+++ src/pages/ProyectDetail/ProyectDetail/ProyectDetail.jsx
@@ -1,18 +1,14 @@
-import "./_proyectDetailDesktop.scss"
-import "./_proyectDetailIpad.scss"
-import "./_proyectDetailMobile.scss"
-
 import { useContext, useEffect } from "react";
 import { OpenNavbarContext } from "../../../context/OpenNavbarContext";
 import NavbarPortfolio from "../../../components/NavbarPortfolio/NavbarPortfolio/NavbarPortfolio";
 import OpenNavbar from "../../../components/OpenNavbar/OpenNavbar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-
-
-
+import { Link } from "react-router-dom";
+import YouTube from 'react-youtube';
 
 export default function ProyectDetail() {
+  const { isMenuOpen, handleExitNavbar } = useContext(OpenNavbarContext);
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -22,25 +18,28 @@ export default function ProyectDetail() {
   };
 
   useEffect(() => {
-    handleExitNavbar()
-  },[]);
+    handleExitNavbar();
+    scrollToTop();
+  }, []);
 
-  scrollToTop()
-
-
-  const { isMenuOpen, handleExitNavbar } = useContext(OpenNavbarContext);
-
-    return (
-        <>
-          <div className="contenedorGralProyectsDetail">
-            {isMenuOpen ? <OpenNavbar /> : <span />}
-            <NavbarPortfolio />
-            <div className="contenedorVolver">
-              <FontAwesomeIcon className='flechaIzquierda' icon={faArrowLeft} />
-              <span>Back</span>
-            </div>
-          </div>
-        </>
-    );
-
+  return (
+    <>
+      <div className="contenedorGralProyectsDetail">
+        {isMenuOpen ? <OpenNavbar /> : <span />}
+        <NavbarPortfolio />
+        <div className="contenedorVolver">
+          <Link to={"/proyects"}>
+            <FontAwesomeIcon className='flechaIzquierda' icon={faArrowLeft} />
+            <span className="back">Back</span>
+          </Link>
+        </div>
+        <div className="contenedorTituloProyect">
+          <h2>Desafío Técnico Mercado Libre</h2>
+        </div>
+        <div className="contenedorVideoProyect">
+          <YouTube className="video" videoId="mUtDxxb85Uk" />
+        </div>
+      </div>
+    </>
+  );
 }
