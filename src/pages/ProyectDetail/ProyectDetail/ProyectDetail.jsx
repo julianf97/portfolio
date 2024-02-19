16153d296189155df75846a96ae9265c7275fedb
Julian Finelli <julianfinelli97@gmail.com>
2024-02-19 09:38:43 -0300

    chore: agregando videos

--- src/pages/ProyectDetail/ProyectDetail/ProyectDetail.jsx
+++ src/pages/ProyectDetail/ProyectDetail/ProyectDetail.jsx
@@ -1,14 +1,20 @@
+import "./_proyectDetailDesktop.scss"
+import "./_proyectDetailIpad.scss"
+import "./_proyectDetailMobile.scss"
+
 import { useContext, useEffect } from "react";
 import { OpenNavbarContext } from "../../../context/OpenNavbarContext";
 import NavbarPortfolio from "../../../components/NavbarPortfolio/NavbarPortfolio/NavbarPortfolio";
+import mercadoLibre from "../../../../public/screensProyect/mercadoLibreScreen.png"
 import OpenNavbar from "../../../components/OpenNavbar/OpenNavbar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import YouTube from 'react-youtube';
+
+
+
 
 export default function ProyectDetail() {
-  const { isMenuOpen, handleExitNavbar } = useContext(OpenNavbarContext);
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -18,28 +24,33 @@ export default function ProyectDetail() {
   };
 
   useEffect(() => {
-    handleExitNavbar();
-    scrollToTop();
-  }, []);
+    handleExitNavbar()
+  },[]);
 
-  return (
-    <>
-      <div className="contenedorGralProyectsDetail">
-        {isMenuOpen ? <OpenNavbar /> : <span />}
-        <NavbarPortfolio />
-        <div className="contenedorVolver">
-          <Link to={"/proyects"}>
-            <FontAwesomeIcon className='flechaIzquierda' icon={faArrowLeft} />
-            <span className="back">Back</span>
-          </Link>
-        </div>
-        <div className="contenedorTituloProyect">
-          <h2>Desafío Técnico Mercado Libre</h2>
-        </div>
-        <div className="contenedorVideoProyect">
-          <YouTube className="video" videoId="mUtDxxb85Uk" />
-        </div>
-      </div>
-    </>
-  );
+  scrollToTop()
+
+
+  const { isMenuOpen, handleExitNavbar } = useContext(OpenNavbarContext);
+
+    return (
+        <>
+          <div className="contenedorGralProyectsDetail">
+            {isMenuOpen ? <OpenNavbar /> : <span />}
+            <NavbarPortfolio />
+            <div className="contenedorVolver">
+              <Link to={"/proyects"}>
+                <FontAwesomeIcon className='flechaIzquierda' icon={faArrowLeft} />
+                <span className="back">Back</span>
+              </Link>
+            </div>
+            <div className="contenedorTituloProyect">
+              <h2>Desafío Técnico Mercado Libre</h2>
+            </div>
+            <div className="contenedorVideoProyect">
+            <iframe src="https://www.youtube.com/embed/mUtDxxb85Uk?si=s3v_NhYRgrm2u5JE" title="YouTube video player"   sandbox="allow-same-origin allow-scripts" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+          </div>
+        </>
+    );
+
 }
